a8bb7fc52aae687bd4c93f14941751611cf7483b
xueqin <xueqin.shen@outlook.com>
2019-07-18 16:14:35 +0800

    20190718

--- learn_pytest/python_examination/test_r9_04.py
+++ learn_pytest/python_examination/test_r9_04.py
@@ -6,3 +6,21 @@ list2=[5,15,25,10]
 print set(list1)-set(list2)
 print set(list1) & set(list2)
 print set(list1) | set(list2)
+
+lis = [2,4,5,6,7]
+
+# for i in lis:
+	
+	
+
+# 	if i % 2 == 0:
+# 		print i, 'run here'
+# 	    lis.remove(i)
+	   
+
+
+ll = [1,2,3,4,5]
+ll.remove(1)
+print ll
+ll.remove(2)
+print ll
