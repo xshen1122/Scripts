be9f96466706b12c42b135565d294b269e9497be
xueqin <xueqin.shen@outlook.com>
2020-05-27 14:04:49 +0800

    0527-1

--- learn_pytest/python_examination/learn_yield.py
+++ learn_pytest/python_examination/learn_yield.py
@@ -4,3 +4,26 @@
 learn what's yield?
 '''
 
+def foo():
+    print("starting...")
+    while True:
+        res = yield 4
+        print("res:",res)
+# g = foo()
+# print(next(g))
+# print("*"*20)
+# print(next(g))
+
+def foo1():
+    print("starting...")
+    while True:
+        res = yield 4
+        print("res:",res)
+
+
+if __name__ == '__main__':
+	
+	g = foo()
+	print(next(g))
+	print("*"*20)
+	print(g.send(7))
--- suanfa/test.py
+++
@@ -1,5 +0,0 @@
-# test.py
-# coding: utf-8
-l1 = [3]
-l1.insert(0,2)
-print l1
