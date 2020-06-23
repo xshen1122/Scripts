707db9020a3506120b45dad87c5c8163fbc7f31d
xueqin <xueqin.shen@outlook.com>
2020-06-23 10:28:24 +0800

    20200617

--- pytest_new/test_class_new.py
+++ pytest_new/test_class_new.py
@@ -7,6 +7,19 @@ from UserDict import UserDict
 1. 继承
 2. 列表推导
 3. 类的调用
+4. 字典UserDict的继承，重写__setitem__, 等同于dict[key]=value
+5. 第三层子类： 
+MP3FileInfo 是 FileInfo 的子类。在设置了一个 MP3FileInfo 类的 name 时,并
+不只是设置 name 关键字 (像父类 FileInfo 所做的),它还要在文件自身内进行搜
+索 MP3 的标记然后填充一整套关键字
+
+现在真正有趣的开始了。设置 mp3file 的 name 关键字触发了 MP3FileInfo 上
+的 __setitem__ 方法 (而不是 UserDict 的),这个方法注意到我们正在用一个
+真实的值来设置 name 关键字,接着调用 self.__parse 。尽管我们完全还没有
+研究过 __parse 方法,从它的输出你可以看出,它设置了其它几个关键字:
+album 、 artist 、 genre 、 title 、 year
+和 comment 。
+
 '''
 
 def stripnulls(data):
@@ -43,7 +56,7 @@ class MP3FileInfo(FileInfo):
                 fsock.close()
                 if tagdata[:3] == "TAG":
                     for tag, (start, end, parseFunc) in self.tagDataMap.items():
-                        self[tag] = parseFunc(tagdata[start:end])
+                        self[tag] = parseFunc(tagdata[start:end]) #在这里调用了__setitem__
         except IOError:
             pass
     '''
@@ -75,12 +88,18 @@ if __name__ == '__main__':
    # for info in (listDirectory('/home/real/Script/pytest_new/music',['.mp3'])):
    #     print info
 
-    for info in listDirectory("/home/real/Script/pytest_new/music", [".mp3"]):
-
-         #Python中调用类的方法，必须与实例绑定，或者调用自身.
-        print "\n".join(["%s=%s" % (k, v) for k, v in info.items()])
-    # mp3 = '/home/real/Script/pytest_new/music/razorback.mp3'
+    # for info in listDirectory("/home/real/Script/pytest_new/music", [".mp3"]):
+    #
+    #      #Python中调用类的方法，必须与实例绑定，或者调用自身.
+    #     print "\n".join(["%s=%s" % (k, v) for k, v in info.items()])
+    mp3 = '/home/real/Script/pytest_new/music/razorback.mp3'
     # fileinfo = FileInfo(mp3)  #获取了一个字典，key为name，value为文件名
     # print fileinfo
     # mp3new = MP3FileInfo(fileinfo)
     # print mp3new
+    mp3file = MP3FileInfo()
+
+    mp3file["name"] = mp3
+    print mp3file
+    print type(MP3FileInfo), type(MP3FileInfo.tagDataMap), type(mp3file)
+    print MP3FileInfo.tagDataMap
